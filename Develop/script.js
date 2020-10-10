10ff00c5e4b9e864168a599c06749a35d463e96e
Grace Kuo <gkuo.github@gmail.com>
2020-10-10 14:48:19 -0600

    created numeric and letter variables

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,38 @@ var confirmLowercase;
 var confirmNumber;
 var confirmCharacter;
 
+//Numeric numbers:
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+// Alphabet:
+alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
