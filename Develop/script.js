6d79912b1f70da6f29da047ab995b86ae3cd900c
Grace Kuo <gkuo.github@gmail.com>
2020-10-10 14:47:04 -0600

    created user variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,20 @@
+// User variables:
+var enter;
+var confirmUppercase;
+var confirmLowercase;
+var confirmNumber;
+var confirmCharacter;
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
