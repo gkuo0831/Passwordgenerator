7c2186c9a2fd90d441e9623d5aea74ebfa4a4eaa
Grace Kuo <gkuo.github@gmail.com>
2020-10-13 11:50:16 -0600

    Created prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -7,3 +7,18 @@ var confirmCharacter = "!#$%&‘()*+,-./:;<=>?@[]^_`{|}~";
 var confirmNumber = "0123456789";
 let userChoice = [];
 var password = [];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  //var password = generatePassword();
+
+  // ask for user input
+  let length = prompt(
+    "What would you like the length of your password to be? Choose between 8-128 characters."
+  );
