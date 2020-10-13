18f9ccd151f8999a92f26fbe2c26a71420a4c932
Grace Kuo <gkuo.github@gmail.com>
2020-10-13 11:49:24 -0600

    re-created user variables to be easier to generate password from

--- Develop/script.js
+++ Develop/script.js
@@ -5,23 +5,5 @@ var confirmUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var confirmLowercase = "abcdefghijklmnopqrstuvwxyz";
 var confirmCharacter = "!#$%&‘()*+,-./:;<=>?@[]^_`{|}~";
 var confirmNumber = "0123456789";
-var userChoice = [];
+let userChoice = [];
 var password = [];
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  // ask for user input
-  generateBtn = parseInt(
-    prompt(
-      "How many characters would you like your password to be? Choose between 8 and 128."
-    )
-  );
-}
